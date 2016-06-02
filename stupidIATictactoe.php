94b2791f9eafb1a7a68bfc5f3ac9546c8d88d118
gnieark <gnieark@free.fr>
2016-06-02 23:35:23 +0200

    radiohead

--- stupidIATictactoe.php
+++ stupidIATictactoe.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST'); 
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 $cases=array("0-0","0-1","0-2","1-0","1-1","1-2","2-0","2-1","2-2");
 //remplir l'array
--- tictactoe.php
+++ tictactoe.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST'); 
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 /*
 * Tic Tac Toe gnieark's IA V2
 * Gnieark 2016
